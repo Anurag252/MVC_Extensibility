e29672ded008bafe1f229de030bdd29c5efd73c3
ADY8 <ADY8@PUN-ANURAGD02>
2019-01-10 09:55:39 +0530

    First Commit

---
+++ Extensibility/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using Extensibility.CustomRoute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Routing;
+using System.Web.Routing;
+
+namespace Extensibility
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var constraintsResolver = new DefaultInlineConstraintResolver();
+            constraintsResolver.ConstraintMap.Add("CustomRoute", typeof(CustomRouteConstraint));
+            constraintsResolver.ConstraintMap.Add("CustomRouteWithParameter", typeof(CustomRouteWithParams));
+            routes.MapMvcAttributeRoutes(constraintsResolver);
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Extensibility/CustomRoute/CustomRouteWithParams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Extensibility.CustomRoute
+{
+    public class CustomRouteWithParams : IRouteConstraint
+    {
+        private readonly string _parameterValue;
+        public CustomRouteWithParams(string parameteValue)
+        {
+            _parameterValue = parameteValue;
+        }
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            object value;
+            if (values.TryGetValue(parameterName, out value) && value != null)
+            {
+                if (value.ToString() == _parameterValue)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
